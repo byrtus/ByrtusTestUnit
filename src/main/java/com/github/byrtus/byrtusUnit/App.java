c3b5643b7a3c7843d428b89cd17164ae2101f860
byrtus <byrtus@wp.pl>
2020-07-17 10:28:25 +0200

    set App class to start testing from AssertionTesting

--- src/main/java/com/github/byrtus/byrtusUnit/App.java
+++ src/main/java/com/github/byrtus/byrtusUnit/App.java
@@ -3,6 +3,7 @@ package com.github.byrtus.byrtusUnit;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world\n");
+        new ByrtusUnit().getMethodsFromClass(AssertionTesting.class);
     }
 }
