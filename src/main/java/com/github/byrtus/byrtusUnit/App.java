f1001d0c20d377ced8d705b6ef8e92ddda10ae84
byrtus <byrtus@wp.pl>
2020-08-26 10:15:00 +0200

    add start sequence method for DependsOn testing in main

--- src/main/java/com/github/byrtus/byrtusUnit/App.java
+++ src/main/java/com/github/byrtus/byrtusUnit/App.java
@@ -4,6 +4,17 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello world\n");
-        new ByrtusUnit().getMethodsFromClass(AssertionTesting.class);
+
+        System.out.println("-----------------------------\n" +
+                "  Testing Assertions Step 1\n" +
+                "-----------------------------\n");
+
+        new ByrtusUnit().getTestAnnotationClasses(AssertionTesting.class);
+
+        System.out.println("-----------------------------\n" +
+                "  Testing DependsOn Step 2\n" +
+                "-----------------------------\n");
+
+        new ByrtusUnit().getTestAnnotationClasses(DependsOnTesting.class);
     }
 }
